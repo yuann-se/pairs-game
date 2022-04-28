0bbb6c1dd96925e8fd69a244b9476744fa2d0c9c
yuann-se <103625509+yuann-se@users.noreply.github.com>
2022-04-28 19:23:48 +0300

    Update main.js

--- v1/main.js
+++ v1/main.js
@@ -1,5 +1,3 @@
-window.addEventListener('DOMContentLoaded', () => {
-
 const givenArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 let shuffledArray;
 
@@ -23,51 +21,55 @@ function shuffle(array) {
 }
 
 shuffledArray = shuffle(givenArray);
+const timer = document.querySelector('.timer');
+let time;
+
+function startTimer() {
+  --i;
+  if (document.querySelectorAll('.is-flipped').length === 16) {
+    clearInterval(time);
+  };
+  if (i >= 0) {
+    timer.textContent = i;
+  } else {
+    cards.forEach((card) => card.classList.remove('is-flipped'));
+    setTimeout(() => {
+      document.querySelector('.row').style.display = 'none';
+      timer.textContent = '';
+      timer.style.display = 'none';
+      newGameBtn.style.display = 'block';
+      document.querySelector('.fail-img').style.display = 'block';
+      clearInterval(time);
+    }, 1000);
+  }
+}
 
 const cards = document.querySelectorAll('.card');
 let lastTwoCards = [];
-const timer = document.querySelector('.timer');
 
-document.querySelector('.new-game-btn').addEventListener('click', () => {
-  document.querySelector('.new-game-btn').style.display = 'none';
-  document.querySelector('.row').style.display = 'flex';
-  timer.style.display = 'block';
-  timer.textContent = 60;
-  let i = 60;
-
-  function startTimer () {
-    --i;
-    if (document.querySelectorAll('.is-flipped').length === 16) {
-      timer.style.display = 'none';
-    };
-    if (i >= 0) {
-      timer.textContent = i;
-    } else {
-      cards.forEach((card) => card.classList.remove('is-flipped'));
-      setTimeout(() => {
-        document.querySelector('.row').style.display = 'none';
-        timer.style.display = 'none';
-        document.querySelector('.fail-img').style.display = 'block';
-      }, 1000)
-    }
-
-  }
-  setInterval(startTimer, 1000);
+const newGameBtn = document.querySelector('.new-game-btn');
 
 
+window.addEventListener('DOMContentLoaded', () => {
 
-
-});
-
-
-
-
-
+  newGameBtn.addEventListener('click', () => {
+    clearInterval(time);
+    cards.forEach((card) => card.classList.remove('is-flipped'));
+    document.querySelector('.fail-img').style.display = 'none';
+    document.querySelector('.success-img').style.display = 'none';
+    newGameBtn.style.display = 'none';
+    document.querySelector('.row').style.display = 'flex';
+    timer.style.display = 'block';
+    timer.textContent = 60;
+    i = 60;
+    time = setInterval(startTimer, 1000);
+  });
 
   for (let i = 0; i < cards.length; ++i) {
     cards[i].children[0].textContent = shuffledArray[i];
     cards[i].setAttribute('id', `${i}`);
   };
+
 
   cards.forEach((card) => {
     card.addEventListener('click', () => {
@@ -77,25 +79,30 @@ document.querySelector('.new-game-btn').addEventListener('click', () => {
         if (document.getElementById(`${lastTwoCards[0]}`).children[0].textContent !== document.getElementById(`${lastTwoCards[1]}`).children[0].textContent) {
           document.getElementById(`${lastTwoCards[0]}`).classList.remove('is-flipped');
           document.getElementById(`${lastTwoCards[1]}`).classList.remove('is-flipped');
-        }
+        };
         lastTwoCards = [];
-      }
+      };
 
       if (lastTwoCards.length < 2) {
         lastTwoCards.push(card.attributes.id.value);
-      }
+      };
 
       if (document.querySelectorAll('.is-flipped').length === 16) {
         setTimeout(() => {
           document.querySelector('.row').style.display = 'none';
           document.querySelector('.success-img').style.display = 'block';
-        }, 1000)
-
-      }
-
-
+          newGameBtn.style.display = 'block';
+          timer.style.display = 'none';
+        }, 1000);
+      };
     })
   })
+
+
+
+
+
+
 
 
 
